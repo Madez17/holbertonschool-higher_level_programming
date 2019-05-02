d3a77fe5cacadab3833662675b545ddcc76acb61
Alexander Ortiz <alex.ortiz.arevalo@gmail.com>
2019-05-01 21:36:57 -0500

    Number

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
+for number in range(0, 100):
     if number <= 9:
-        print("{} {}".format(number, number), end='')
+        print("0{}, ".format(number), end='')
+    else:
+        print("{}, ".format(number), end='')
