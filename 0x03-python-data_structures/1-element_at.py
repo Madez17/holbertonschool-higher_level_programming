ac58f98dc65ce4dca69c8c8470acc12192068db6
Madez17 <781@holbertonschool.com>
2019-05-07 21:52:12 +0000

    Fixed retrieves an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if my_list is not None:
-        for x in my_list[0:idx + 1]:
-            if idx < 0 or idx >= len(my_list):
-                return None
-        return (x)
+        if idx < 0 or idx >= len(my_list):
+            return None
+        else:
+            return my_list[idx]
