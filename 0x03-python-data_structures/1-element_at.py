fd00f02acfcab62670cec0586318079a9075ccb7
Madez17 <781@holbertonschool.com>
2019-05-07 20:57:41 +0000

    Fixed: retrieves and element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,5 @@ def element_at(my_list, idx):
     for x in my_list[idx:-1]:
         if idx < 0 or idx >= len(my_list):
             return None
-        return (x)
+        else:
+            return (x)
