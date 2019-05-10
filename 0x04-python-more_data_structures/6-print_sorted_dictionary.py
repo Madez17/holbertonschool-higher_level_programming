ab01753a23011b0fefc10214533de57d7b4325bf
Madez17 <781@holbertonschool.com>
2019-05-10 15:06:46 +0000

    prints a dictionary by ordered keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for num in sorted(a_dictionary):
-        print(num, a_dictionary[num])
+        print('{}: {}'.format(num, a_dictionary[num]))
