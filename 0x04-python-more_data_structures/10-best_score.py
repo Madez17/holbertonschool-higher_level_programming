5681129a0a849c6da30626cfdc72e1a5bd952351
Madez17 <781@holbertonschool.com>
2019-05-12 06:55:56 +0000

    Max vale of a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        axim = max(a_dictionary, key=a_dictionary.get)
+        maxim = max(a_dictionary, key=a_dictionary.get)
         return maxim
