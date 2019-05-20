ca137997ea3a64940a2e874c0361fa0fa9b7beff
Madez17 <781@holbertonschool.com>
2019-05-20 20:29:59 +0000

    Adjust First exercise[Print list]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for iter1 in range(x):
             if iter1 < x:
-                print('{:d}'.format(my_list[iter1]), end='')
+                print('{}'.format(my_list[iter1]), end='')
     except Exception as ex:
         print()
         return iter1
     print()
     return iter1 + 1
-        
