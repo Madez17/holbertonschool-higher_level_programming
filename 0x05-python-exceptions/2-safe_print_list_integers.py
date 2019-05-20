8d62d55c106aab22f6f2899476708a35233eb6a2
Madez17 <781@holbertonschool.com>
2019-05-20 22:11:12 +0000

    print number only integer of list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for iter1 in range(x):
+        try:
+            if iter1 < x:
+                print('{:d}'.format(my_list[iter1]), end='')
+                count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
