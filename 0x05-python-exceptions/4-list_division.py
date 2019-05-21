bebf866bd1ed7ac731506a4e6ba23acffaacfb82
Madez17 <781@holbertonschool.com>
2019-05-21 00:46:03 +0000

    Division two list and use try/except/finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    newList = []
+    for x in range(list_length):
+        try:
+            divTwoList = my_list_1[x] / my_list_2[x]
+        except ZeroDivisionError as error:
+            divTwoList = 0
+            print("division by 0")
+        except TypeError as te:
+            divTwoList = 0
+            print("wrong type")
+        except IndexError as idx:
+            divTwoList = 0
+            print("out of range")
+        finally:
+            newList.append(divTwoList)
+    return newList
