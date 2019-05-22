a03529b31c41855e5e56d01a7f0762d456cbfd66
Madez17 <781@holbertonschool.com>
2019-05-22 03:54:02 +0000

    Create a public method atributte and return os square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,9 +2,12 @@
 class Square(object):
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError ('size must be an integer')
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError ('size must be >= 0')
-        def area(self):
-            save = Square(size)
-            return size
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        saveSquareArea = self.__size * self.__size
+        return saveSquareArea
