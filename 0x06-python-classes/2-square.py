e882db7edda25b024d78d3e8ff11076ff3c2a1d6
Madez17 <781@holbertonschool.com>
2019-05-22 00:42:33 +0000

    second[definicion of object class and verify parameter integer and raise error conditions]

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,8 +3,8 @@ class Square(object):
     def __init__(self, size=0):
         try:
             if type(size) != int:
-                raise TypeError ('size must be a integer')
+                raise TypeError('size must be a integer')
             elif size < 0:
-                raise ValueError ('size must be >= 0')
+                raise ValueError('size must be >= 0')
         finally:
             self.__size = size
