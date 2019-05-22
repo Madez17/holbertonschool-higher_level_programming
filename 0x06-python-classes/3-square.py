8eb0d38c78e550c7342570626e1979e88df5ba5f
Madez17 <781@holbertonschool.com>
2019-05-22 00:40:23 +0000

    Update excersise firs[Define a private instance atributte], and second[definicion of object class and verify parameter integer and raise error conditions]

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class Square(object):
+    def __init__(self, size=0):
+        try:
+            if type(size) != int:
+                raise TypeError ('size must be an integer')
+            else size < 0:
+                raise ValueError ('size must be >= 0')
+        finally:
+            self.__size = size
+        def area(self)
+            return area.__self
