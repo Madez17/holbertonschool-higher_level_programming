2125c2e670a8b625eaf077c1d2b9d1982969a83d
Madez17 <781@holbertonschool.com>
2019-05-24 05:35:56 +0000

    Function that return two numbers assd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) != int:
-        raise TypeError ('a must be an integer')
+    if type(a) is float or type(b) is float:
+        a = int(a)
+        b = int(b)
+    elif type(a) != int:
+        raise TypeError ('a must be an integer') 
     elif type(b) != int:
-        raise TypeError 
-    elif type(a, b) != float:
-        raise TypeError
-    
+        raise TypeError ('b must be an integer') 
     return a + b
     
