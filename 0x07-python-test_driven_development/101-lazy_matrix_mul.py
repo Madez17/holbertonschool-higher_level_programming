32624f0620ade4420fb85616b3c30bb25d64ffc3
Madez17 <781@holbertonschool.com>
2019-05-26 00:55:10 -0500

    numpy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module text_identation
+
+This Module you can pass a text how parameter and validate if this text
+has some characters . ? : if the text found that the function do a space_line
+and print the text in the next liene and the cahracteres too.
+
+"""
+
+
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """Example function text identation
+
+    Args:
+        text: Unique parameter.
+
+    Print:
+        Print pharagraph.
+
+    """
+    result = np.dot(m_a, m_b)
+    print(result)
