354f9963bdf74812fc6a06592ad1b64d6a6c5d3e
Madez17 <781@holbertonschool.com>
2019-06-04 22:17:09 +0000

    Only sub class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""Module inherits """
+
+
 def inherits_from(obj, a_class):
+    """Module is same class
+    Args: obj
+    Print: a_class
+    Return: True
+    """
+    if type(obj) == a_class:
+        return False
+    else:
+        return issubclass(type(obj), a_class)
