6cd69e1bde96e4f264d553da6fdb99e22c49b4b8
Madez17 <781@holbertonschool.com>
2019-06-04 22:01:14 +0000

    Same class inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""Module is kind of class"""
+
+
 def is_kind_of_class(obj, a_class):
+    """Module is same class
+    Args: obj
+    Print: a_class
+    Return: True
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
