457bf4d8afc3aa3e7f5fe4936859bcf22777866b
Madez17 <781@holbertonschool.com>
2019-06-06 04:42:45 +0000

    dictionary description with simple data structure

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,7 +2,7 @@
 def read_lines(filename="", nb_lines=0):
     with open(filename, mode='r', encoding='UTF8') as MyFile:
         counLines = 0
-        for line in MyFile:
+        for line in range(nb_lines):
             counLines += 1
             print(MyFile.readline(), end='')
         if nb_lines <= 0 or nb_lines >= counLines:
