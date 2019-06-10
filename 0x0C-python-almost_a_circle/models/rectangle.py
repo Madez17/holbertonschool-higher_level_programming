ff412456caef274587ca76e5bd87107761aed858
Madez17 <781@holbertonschool.com>
2019-06-10 21:33:15 +0000

    Update rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,36 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def update(self, *args, **kwargs):
+        """Constructor update class rectangle"""
+        for iter1 in range(len(args)):
+            if iter1 == 0:
+                Base.__init__(self, args[iter1])
+
+            elif iter1 == 1:
+                self.width = args[iter1]
+
+            elif iter1 == 2:
+                self.height = args[iter1]
+
+            elif iter1 == 3:
+                self.x = args[iter1]
+
+            else:
+                self.y = args[iter1]
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    Base.__init__(self, value)
+                elif key == 'width':
+                    self.width = value
+                elif key == 'height':
+                    self.height = value
+                elif key == 'x':
+                    self.x = value
+                else:
+                    self.y = value
+
     def __str__(self):
         """ Method str show objects """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
