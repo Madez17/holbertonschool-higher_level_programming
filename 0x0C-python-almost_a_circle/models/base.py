b4a005bd9c7352db3e51f23e38cb683d066c248a
Madez17 <781@holbertonschool.com>
2019-06-11 19:28:52 +0000

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-import json
 """ Base Module"""
+import json
 
 
 class Base(object):
     """Class definition - Base"""
     __nb_objects = 0
 
-    """def from_json_string(json_string):"""
+    def __init__(self, id=None):
+        """Class - onstructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -20,22 +26,12 @@ class Base(object):
     @classmethod
     def save_to_file(cls, list_objs):
         """Class method save objects in new file"""
-        file_saved_obj = cls.__name__ + '.json'
 
-        """if list_objs is None:
-            with open(file_saved_obj, mode='w', encoding='UTF8') as MyFile
-                MyFile.write(json.dumps())"""
+        file_saved_obj = cls.__name__ + '.json'
         newList = []
+
         for obj in list_objs:
             newList.append(obj.to_dictionary())
 
         with open(file_saved_obj, mode='w', encoding='UTF8') as MyFile:
             MyFile.write(Base.to_json_string(newList))
-
-    def __init__(self, id=None):
-        """Class - onstructor """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
