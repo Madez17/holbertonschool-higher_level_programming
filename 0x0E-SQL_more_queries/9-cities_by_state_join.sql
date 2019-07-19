a6fa9fdb9d8389b2d915ff4cf27562c67455ee15
Madez <781@holbertonschool.com>
2019-07-19 00:10:08 +0000

    [INNER JOIN] - UNIR 2 TABLES

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Inner Join
-SELECT cities.id, states.name, cities.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC;
