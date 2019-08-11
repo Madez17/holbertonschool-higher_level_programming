5e3545a4ca8bbfc9d4b1fc65aa3c041d6affc3f2
Madez17 <781@holbertonschool.com>
2019-08-11 05:37:51 +0000

    [CReate] - table using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 class State(Base):
-    __tablename__ = 'State'
+    __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
