a477750c79c95ca670924e7a6922cf611ecc87fb
Madez17 <781@holbertonschool.com>
2019-08-11 23:59:48 +0000

    [Create] - script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.query(State).filter(State.id == 2).
+    session.query(State).filter(State.id == 2).\
     update({State.name: "New Mexico"})
     session.commit()
 
