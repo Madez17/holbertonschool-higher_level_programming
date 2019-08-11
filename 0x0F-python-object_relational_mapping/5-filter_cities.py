c9edfaa83d8466f33d7730d971a1c8a330e14211
Madez17 <781@holbertonschool.com>
2019-08-11 04:48:50 +0000

    [Fixed] - Inner join

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("""SELECT cities.name FROM cities INNER JOIN states ON
                    cities.state_id=states.id
-                   WHERE states.name=%s;""", (argv[4],))
+                   WHERE states.name=%s""", (argv[4],))
     resc = cursor.fetchall()
     for iter in resc:
         if iter is not resc[len(resc) - 1]:
