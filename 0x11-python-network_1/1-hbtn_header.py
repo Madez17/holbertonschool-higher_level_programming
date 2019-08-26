c7aad529aece122eddd098b8bdb9ec081022c249
Madez17 <781@holbertonschool.com>
2019-08-26 23:52:36 +0000

    [Fixed] - comments

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id"""
+# script that takes in a URL, sends a request to the URL display X-Request-Id
 
 import urllib.request
 from sys import argv
