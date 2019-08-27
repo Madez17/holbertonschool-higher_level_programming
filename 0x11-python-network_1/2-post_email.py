b3bba68331e56c428ae373672b88ddebaa6ac931
Madez17 <781@holbertonschool.com>
2019-08-27 00:08:57 +0000

    [POST] - sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id"""
+# Script sends a POST request to the passed URL with the email as a parameter
 
 import urllib.parse
 import urllib.request
 from sys import argv
 
-url = argv[1]
-params = {"value": argv[2]}
-query_string = urllib.parse.urlencode(params).encode("ascii")
 
 if __name__ == "__main__":
+
+    url = argv[1]
+    params = {"email": argv[2]}
+    query_string = urllib.parse.urlencode(params).encode('ascii')
     with urllib.request.urlopen(url, query_string) as response:
         response_text = response.read().decode('utf-8')
         print(response_text)
