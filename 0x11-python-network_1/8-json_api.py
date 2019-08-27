eb91cb1b55daa8abc1e038ff4b4089412fb0c0ad
Madez17 <781@holbertonschool.com>
2019-08-27 04:18:37 +0000

    Python script that takes in a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python script that takes in a letter and sends a POST request 
+# Python script that takes in a letter and sends a POST request
 
 import requests
 from sys import argv
