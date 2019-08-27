7ce0f5a1550c0fa2f37a040ce7b501f50b6b4ac9
Madez17 <781@holbertonschool.com>
2019-08-27 02:58:47 +0000

    [status code 400] - success validate request

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# sends a request to the URL and displays the body of the response
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    response = requests.get(url)
+
+    if response.status_code <= 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
