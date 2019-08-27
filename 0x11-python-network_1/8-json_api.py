815747d4b85971b91b23895ca52439585addecfd
Madez17 <781@holbertonschool.com>
2019-08-27 04:17:04 +0000

    Python script that takes in a letter and sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Python script that takes in a letter and sends a POST request 
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ""
+
+    if len(argv) > 1:
+        q = argv[1]
+
+    params = {'q': q}
+    response = requests.post(url, params)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'],
+                                   json_response['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
