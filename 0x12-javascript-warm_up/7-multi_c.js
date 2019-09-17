aa2b519ebe7dd9851e191baf7cdea20d60ca1e81
Madez17 <mafe.mendez.m@gmail.com>
2019-09-17 22:19:44 +0000

    [x] - tiemes console.log

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   let i = 0;
-  while ( i < process.argv[2]) {
+  while (i < process.argv[2]) {
     console.log('C is fun');
     i++;
   }
