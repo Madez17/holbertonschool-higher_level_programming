19b043b7f379ef6a07931b6b65a9ada3f41f0046
Madez17 <781@holbertonschool.com>
2019-09-18 04:25:27 +0000

    Fixed sintax

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 if (process.argv.length > 3) {
   let numbers = process.argv.slice(2);
-  numbers = numbers.sort(function(num1, num2) {
+  numbers = numbers.sort(function (num1, num2) {
     return num2 - num1;
   });
   console.log(numbers[1]);
