6bd52406e09aa4e52bc8bdccfb14ea3bef18deba
Madez17 <mafe.mendez.m@gmail.com>
2019-09-18 00:37:00 +0000

    Fixed- if parameter are negative

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
-} else if (process.argv[2] > 0) {
+} else if (parseInt(process.argv[2] > 0)) {
   let i = 0;
-  while (i < process.argv[2]) {
-    console.log('x'.repeat(process.argv[2]));
+  while (i < parseInt(process.argv[2])) {
+    console.log('x'.repeat(parseInt(process.argv[2])));
     i++;
   }
 }
