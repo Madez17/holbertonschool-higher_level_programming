d8bea86491e0844fd0294d157c7eb0b5c85b3f77
Madez <781@holbertonschool.com>
2019-09-24 23:09:23 +0000

    Write file access file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@
 const fs = require('fs');
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
-    if (error) {
-            console.log(err)
-    }
-    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
     if (err) {
-       console.log(err);
-     }
-    });
+      console.log(err);
+    }
+  });
 });
