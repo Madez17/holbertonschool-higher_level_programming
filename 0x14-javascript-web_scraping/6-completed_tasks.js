1c95baa48ae5dcbeef722229a5eaca2e3cc6025e
Madez <781@holbertonschool.com>
2019-09-25 00:57:38 +0000

    [print ]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,13 +5,13 @@ const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
-  } else { 
-    let list = JSON.parse(body);
-    let dict = {};
+  } else {
+    const list = JSON.parse(body);
+    const dict = {};
 
     for (let i = 0; i < list.length; i++) {
-      if (list[i].completed === true) {  
-        let user = list[i].userId;
+      if (list[i].completed === true) {
+        const user = list[i].userId;
         if (user in dict) {
           dict[user]++;
         } else {
@@ -20,5 +20,5 @@ request.get(process.argv[2], function (error, response, body) {
       }
     }
     console.log(dict);
-  }  
+  }
 });
