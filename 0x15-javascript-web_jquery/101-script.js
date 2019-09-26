3fdbe88dd709004fa3e618ceb0d707141e23a886
Madez <781@holbertonschool.com>
2019-09-26 23:05:06 +0000

    [methods] add remove and clear

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#add_item').click(function () {
+    $('<li>Item</li>').appendTo('UL.my_list');
+  });
+  $('#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+  $('#clear_list').click(function () {
+    $('UL.my_list li').remove();
+  });
+});
